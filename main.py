d66eef32be5c7274ea784caec1a99dadc72911dd
Are-You-a-Person <109112940+Are-You-a-Person@users.noreply.github.com>
2024-05-25 17:46:58 -0700

    Update main.py
    Less requests, more optimized

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-import datetime
+from datetime import datetime, timedelta
 import threading
 import random
 import os
@@ -21,7 +21,7 @@ projects_to_protect = ["108566337"]
 # Cloud fun 3: "823872487"
 # Minecraft-ish: "843162693"
 
-bad_users = ["-Plat-", "CoolBotABC123AWESOME", "shj_bot", "CorrectScratcher", "ns92", "betterconnection10", "superordi", "Vrai_nom", "SpartanDav", "WAYLIVES", "ahlashaool", "TheMobileGames"]  # exact capitalization matters; I am 100% sure they hacked.
+bad_users = ["-Plat-", "CoolBotABC123AWESOME", "shj_bot", "CorrectScratcher", "ns92", "betterconnection10", "superordi", "Vrai_nom", "SpartanDav", "WAYLIVES", "ahlashaool", ""]  # exact capitalization matters; I am 100% sure they hacked.
 
 test_ban_for_fun = [""]
 # a second ban list used to target specific hackers for specific reasons
@@ -42,7 +42,7 @@ conns_to_disconnect = []
 new_conns_to_use = []
 for project in projects_to_protect:
     new_conns_to_use.append(session.connect_cloud(project))
-    print("new conn to use:", project)
+    print("New conn to use:", project)
 
 
 def create_new_conns():
@@ -67,7 +67,7 @@ update_conns_thread.start()
 conns = []
 for project in projects_to_protect:
     conns.append(session.connect_cloud(project))
-    print("main conn:", project)
+    print("Main conn:", project)
 
 events = []
 for project in projects_to_protect:
@@ -83,12 +83,18 @@ total_success_count = [0 for _ in range(len(projects_to_protect))]
 time_at_last_event = [0 for _ in range(len(projects_to_protect))]
 time_at_last_success = [0 for _ in range(len(projects_to_protect))]
 print(attempted_event_count, successes, total_event_count, total_success_count, time_at_last_event, time_at_last_success)
+recent_veified_users_dict = {}
 
 for i in events:
     @i.event  # Called when the event listener is ready
     def on_ready():
         print("Event listener ready:", i)
         print("Hacker detection is ready for", projects_to_protect[events.index(i)])
+
+
+def get_PDT_time():
+    return (datetime.utcnow() + timedelta(hours=-7)).strftime('%Y-%m-%d %H:%M:%S PDT')
+
 
 for i in events:
     @i.event
@@ -104,27 +110,33 @@ for i in events:
         global override_value
         global override_value_for_fun
         index = events.index(i)
+        user = scratch3.get_user(event.user)
         if use_random_value == True:
             override_value = random.randint(10**(256-1), 10**256 - 1)
             override_value_for_fun = override_value
-        iso_date = session.connect_user(event.user).join_date
-        if ((time.time() - (datetime.datetime.strptime(iso_date, "%Y-%m-%dT%H:%M:%S.%fZ").timestamp())) < 1209600) and (event.user not in whitelisted_users):  # if the user was made less than 2 weeks ago, their cloud vars will be overidden
-            conns[index].set_var(event.var, override_value)
-            attempted_event_count[index] += 1
-            time_at_last_event[index] = time.monotonic()
-            print("New Scratcher detected:", event.user)
-        elif (event.user in bad_users) and (event.user not in whitelisted_users):
-            for _ in range(1):
+        if (event.user not in whitelisted_users) and ((event.user not in recent_veified_users_dict) or (int(time.time()) > recent_veified_users_dict[event.user] + 120)):
+            if event.user in bad_users:
                 conns[index].set_var(event.var, override_value)
-            attempted_event_count[index] += 1
-            time_at_last_event[index] = time.monotonic()
-            print("Bad user detected:", event.user)
-        elif ((event.user in test_ban_for_fun) or ("*" in test_ban_for_fun)) and (event.user not in whitelisted_users):
-            # conns[index].set_var(event.var, override_value_for_fun)
-            conns[index].set_var(event.var, override_value_for_fun)
-            attempted_event_count[index] += 1
-            time_at_last_event[index] = time.monotonic()
-            print("Test ban for fun user detected in", projects_to_protect[index], ":", event.user)
+                attempted_event_count[index] += 1
+                time_at_last_event[index] = time.monotonic()
+                print("Bad User detected:", event.user, "at", get_PDT_time())
+            elif user.does_exist() == False:
+                conns[index].set_var(event.var, override_value)
+                attempted_event_count[index] += 1
+                time_at_last_event[index] = time.monotonic()
+                print("Banned User detected:", event.user, "at", get_PDT_time())
+            elif user.is_new_scratcher():
+                conns[index].set_var(event.var, override_value)
+                attempted_event_count[index] += 1
+                time_at_last_event[index] = time.monotonic()
+                print("New Scratcher detected:", event.user, "at", get_PDT_time())
+            else:
+                recent_veified_users_dict.update({event.user: int(time.time())}) #prevents constantly making requests to Scratch to check users
+            if (event.user in test_ban_for_fun) or ("*" in test_ban_for_fun):
+                conns[index].set_var(event.var, override_value_for_fun)
+                attempted_event_count[index] += 1
+                time_at_last_event[index] = time.monotonic()
+                print("Test ban for fun user detected:", event.user, "at", get_PDT_time())
 
         if (event.value == override_value) or (event.value == override_value_for_fun):
             successes[index] += 1
